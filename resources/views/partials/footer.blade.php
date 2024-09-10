9dffc2aef3c223c60ccc4ebad92aa3c8c468d793
Francescosommellaa <franc.sommella@gmail.com>
2024-09-10 21:26:44 +0200

    add img e clonate e adattato footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,54 @@
 <footer class="my-5 text-center">
-    <p>by UdU</p>
+    <div class="top-nav">
+        <ul class="">
+            <li v-for="(item, index) in topFooterItems">
+                <img src="{{ $item['img'] }}" alt="{{ $item['title'] }}">
+                <h5>{{ $item['title'] }}</h5>
+            </li>
+        </ul>
+    </div>
+
+    <div class="footer">
+        <div class="link-utili">
+            <div>
+                <h4>DC Comics</h4>
+                <ul v-for="(item, index) in dcComicsItems">
+                    @foreach ($dcComicsItems as $item)
+                        <li><a href="{{ $item['link'] }}">{{ $item['link'] }}</a></li>
+                    @endforeach
+                </ul>
+                <h4>Shop</h4>
+                <ul>
+                    @foreach ($shopItems as $item)
+                        <li><a href="{{ $item['link'] }}">{{ $item['link'] }}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <h4>Shop</h4>
+            <ul>
+                @foreach ($shopItems as $item)
+                    <li><a href="{{ $item['link'] }}">{{ $item['link'] }}</a></li>
+                @endforeach
+            </ul>
+
+            <h4>DC</h4>
+            <ul>
+                @foreach ($dcItems as $item)
+                    <li><a href="{{ $item['link'] }}">{{ $item['link'] }}</a></li>
+                @endforeach
+            </ul>
+
+            <h4>Siti</h4>
+            <ul>
+                @foreach ($sitesItem as $item)
+                    <li><a href="{{ $item['link'] }}">{{ $item['link'] }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+    <div>
+        <img src="../assets/img/dc-logo-bg.png" alt="">
+    </div>
+    </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,75 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     $title = 'Layout base - HOME';
     $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, iusto dolore eaque laudantium optio, adipisci perspiciatis voluptate obcaecati sit perferendis natus velit ut! Tempora voluptatum ipsam modi rem voluptas.';
+
+    // Array per il footer
+    $topFooterItems = [
+        [
+            'img' => asset('img/buy-comics-digital-comics.png'),
+            'title' => 'DIGITAL COMICS'
+        ],
+        [
+            'img' => asset('img/buy-comics-merchandise.png'),
+            'title' => 'DC MERCHANDISE'
+        ],
+        [
+            'img' => asset('img/buy-comics-subscriptions.png'),
+            'title' => 'SUBSCRIPTION'
+        ],
+        [
+            'img' => asset('img/buy-comics-shop-locator.png'),
+            'title' => 'COMIC SHOP LOCATOR'
+        ],
+        [
+            'img' => asset('img/buy-dc-power-visa.svg'),
+            'title' => 'DC POWER VISA'
+        ]
+    ];
+
+    // Array per la sezione DC Comics
+    $dcComicsItems = [
+        ['link' => 'characters'],
+        ['link' => 'comics'],
+        ['link' => 'movies'],
+        ['link' => 'tv'],
+        ['link' => 'games'],
+        ['link' => 'videos'],
+        ['link' => 'news'],
+    ];
+
+    // Array per la sezione Shop
+    $shopItems = [
+        ['link' => 'shop-dc'],
+        ['link' => 'shop-merchandise'],
+    ];
+
+    // Array per la sezione DC
+    $dcItems = [
+        ['link' => 'terms-of-use'],
+        ['link' => 'privacy-policy'],
+        ['link' => 'ad-choices'],
+        ['link' => 'advertising'],
+        ['link' => 'jobs'],
+        ['link' => 'subscriptions'],
+        ['link' => 'talent-workshops'],
+        ['link' => 'CPSC-certificates'],
+        ['link' => 'ratings'],
+        ['link' => 'shop-help'],
+        ['link' => 'contact-us'],
+    ];
+
+    // Array per la sezione Siti
+    $sitesItem = [
+        ['link' => 'DC'],
+        ['link' => 'MAD-magazine'],
+        ['link' => 'DC-kids'],
+        ['link' => 'DC-universe'],
+        ['link' => 'DC-power-visa'],
+    ];
     return view('home', compact('text', 'title'));
 })->name('home');
 
